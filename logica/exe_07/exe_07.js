547298fccd1342495a370c00a6b4d05092797521
“Alexandre <dev.alexandre.lopes@gmail.com>
2021-03-02 14:33:09 -0400

    Atualizando o curso de logica

--- logica/exe_07/exe_07.js
+++ logica/exe_07/exe_07.js
@@ -9,3 +9,15 @@ Checar se um número é realmente um número - Retorna o próprio número
 Use a função com números de 0 a 100
 
 */
+
+function FizzBuzz(n) {
+    if (typeof n !== 'number') return n;
+    if (n % 3 == 0 && n % 5 == 0) return 'FizzBuzz';
+    if (n % 3 == 0)return 'Fizz';
+    if (n % 5 == 0)return 'Buzz';
+    return n;
+}
+
+for (i=0; i<=100; i++){
+    console.log(i,FizzBuzz(i))
+}
